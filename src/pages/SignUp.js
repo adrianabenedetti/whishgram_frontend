4ee310cedf81f9010c9de05211d8a2e6a8598053
Adriana Benedetti <adrianabenedetti@192.168.1.3>
2023-07-14 00:14:57 +0200

    edit sign up page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,114 +1,122 @@
 import React from 'react';
 import { useState } from 'react';
-import Form from 'react-bootstrap/form';
-import Button from 'react-bootstrap/button';
-import Container from 'react-bootstrap/container';
+import { Col, Button, Row, Container, Card, Form } from "react-bootstrap";
 
 
 const SignUp = () => {
-    
-    const [formData, setFormData] = useState({
-        firstName: "",
-        lastName: "",
-        userName: "",
-        email: "",
-        password: ""
-    });
-    console.log(formData);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    userName: "",
+    email: "",
+    password: "",
+  });
+  console.log(formData);
 
-    const handleChange = (e) => {
-        setFormData((prev) => ({
-            ...prev,
-            [e.target.name]: e.target.value
-        }));
-    }; 
+  const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
+  };
 
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-        const req = await fetch("http://localhost:5050/users/new", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(formData),
-        });
-        const user = await req.json();
-        if (req.ok) {
-          localStorage.setItem("loggedIn", JSON.stringify(user));
-        }
-        resetFields()
-        return user;
-      } catch (error) {
-        console.log(error);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const req = await fetch("http://localhost:5050/users/new", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const user = await req.json();
+      if (req.ok) {
+        localStorage.setItem("loggedIn", JSON.stringify(user));
       }
-    }; 
-
-    const resetFields = () => {
-        setFormData({
-            firstName: "",
-            lastName: "",
-            userName: "",
-            email: "",
-            password: ""
-        })
+      resetFields();
+      return user;
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  const resetFields = () => {
+    setFormData({
+      firstName: "",
+      lastName: "",
+      userName: "",
+      email: "",
+      password: "",
+    });
+  };
 
   return (
-    <Container class="d-flex flex-column align-items-center">
-        <h3 className='pt-5 mt-5'>Sign Up</h3>
-      <Form className='w-25 pt-3 mt-3 align-items-center' onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="firstName"
-            onChange={handleChange}
-            value={formData.firstName}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control
-            type="text"
-            name="lastName"
-            onChange={handleChange}
-            value={formData.lastName}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            name="userName"
-            onChange={handleChange}
-            value={formData.userName}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            name="email"
-            onChange={handleChange}
-            value={formData.email}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            name="password"
-            onChange={handleChange}
-            value={formData.password}
-          />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+    <Container>
+      <Row className="vh-100 d-flex justify-content-center align-items-center">
+        <Col md={8} lg={6} xs={12}>
+          <Card className="shadow">
+            <Card.Body>
+              <div className="mb-3 mt-md-4">
+                <h2 className="fw-bold mb-2 text-center">Login</h2>
+                <div className="mb-3"></div>
+                <Form onSubmit={handleSubmit}>
+                  <Form.Group className="mb-3">
+                    <Form.Label>First Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="firstName"
+                      onChange={handleChange}
+                      value={formData.firstName}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Last Name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="lastName"
+                      onChange={handleChange}
+                      value={formData.lastName}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control
+                      type="text"
+                      name="userName"
+                      onChange={handleChange}
+                      value={formData.userName}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      name="email"
+                      onChange={handleChange}
+                      value={formData.email}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      name="password"
+                      onChange={handleChange}
+                      value={formData.password}
+                    />
+                  </Form.Group>
+                  <Button variant="primary" type="submit" className='text-align-center'>
+                    Submit
+                  </Button>
+                </Form>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
     </Container>
   );
 }
