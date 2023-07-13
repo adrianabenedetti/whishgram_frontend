06c62ed0d4a6acf115c22ce5d079f5f86554b1ad
Adriana Benedetti <adrianabenedetti@MacBook-Air-di-Adriana.local>
2023-07-13 14:07:22 +0200

    initial commit homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/home';
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path='/' element={<Home/>} />
+      <Route path='/Signup' element={<Signup/>} />
+      <Route path='/Login' element={<Login/>} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
